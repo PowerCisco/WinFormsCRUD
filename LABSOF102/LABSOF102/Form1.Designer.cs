10d76317befb90ac6cc0600c4a14b179bdc1c772
Francisco Castillo <francisco.castillo@outlook.com>
2021-03-06 18:30:00 -0400

    updated control properties, added code to calculate notes.

--- LABSOF102/LABSOF102/Form1.Designer.cs
+++ LABSOF102/LABSOF102/Form1.Designer.cs
@@ -29,13 +29,359 @@ namespace LABSOF102
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtPrimerParcial = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtCreditos = new System.Windows.Forms.TextBox();
+            this.txtLiteral = new System.Windows.Forms.TextBox();
+            this.txtNotaFinal = new System.Windows.Forms.TextBox();
+            this.txtExamenFinal = new System.Windows.Forms.TextBox();
+            this.txtPracticas = new System.Windows.Forms.TextBox();
+            this.txtLaboratorio = new System.Windows.Forms.TextBox();
+            this.txtMatricula = new System.Windows.Forms.TextBox();
+            this.txtSegundoParcial = new System.Windows.Forms.TextBox();
+            this.txtAsignatura = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(358, 39);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 10;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(358, 77);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 11;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(358, 164);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 14;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(358, 135);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 13;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(358, 106);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 12;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // txtPrimerParcial
+            // 
+            this.txtPrimerParcial.Location = new System.Drawing.Point(103, 182);
+            this.txtPrimerParcial.Name = "txtPrimerParcial";
+            this.txtPrimerParcial.Size = new System.Drawing.Size(100, 20);
+            this.txtPrimerParcial.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Matricula";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(209, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Creditos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 344);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Nota Final";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 375);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Literal";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Segundo Parcial";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Primer Parcial";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 158);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Asignatura";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 127);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Apellido";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 96);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Nombre";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 313);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 13);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Examen Final";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 282);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(51, 13);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Practicas";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(12, 251);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(60, 13);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "Laboratorio";
+            // 
+            // txtCreditos
+            // 
+            this.txtCreditos.Location = new System.Drawing.Point(260, 151);
+            this.txtCreditos.MaxLength = 2;
+            this.txtCreditos.Name = "txtCreditos";
+            this.txtCreditos.ReadOnly = true;
+            this.txtCreditos.Size = new System.Drawing.Size(21, 20);
+            this.txtCreditos.TabIndex = 19;
+            this.txtCreditos.TabStop = false;
+            // 
+            // txtLiteral
+            // 
+            this.txtLiteral.Location = new System.Drawing.Point(103, 368);
+            this.txtLiteral.MaxLength = 1;
+            this.txtLiteral.Name = "txtLiteral";
+            this.txtLiteral.ReadOnly = true;
+            this.txtLiteral.Size = new System.Drawing.Size(21, 20);
+            this.txtLiteral.TabIndex = 20;
+            // 
+            // txtNotaFinal
+            // 
+            this.txtNotaFinal.Location = new System.Drawing.Point(103, 337);
+            this.txtNotaFinal.Name = "txtNotaFinal";
+            this.txtNotaFinal.ReadOnly = true;
+            this.txtNotaFinal.Size = new System.Drawing.Size(21, 20);
+            this.txtNotaFinal.TabIndex = 9;
+            // 
+            // txtExamenFinal
+            // 
+            this.txtExamenFinal.Location = new System.Drawing.Point(103, 306);
+            this.txtExamenFinal.Name = "txtExamenFinal";
+            this.txtExamenFinal.Size = new System.Drawing.Size(100, 20);
+            this.txtExamenFinal.TabIndex = 8;
+            // 
+            // txtPracticas
+            // 
+            this.txtPracticas.Location = new System.Drawing.Point(103, 275);
+            this.txtPracticas.Name = "txtPracticas";
+            this.txtPracticas.Size = new System.Drawing.Size(100, 20);
+            this.txtPracticas.TabIndex = 7;
+            // 
+            // txtLaboratorio
+            // 
+            this.txtLaboratorio.Location = new System.Drawing.Point(103, 244);
+            this.txtLaboratorio.Name = "txtLaboratorio";
+            this.txtLaboratorio.Size = new System.Drawing.Size(100, 20);
+            this.txtLaboratorio.TabIndex = 6;
+            // 
+            // txtMatricula
+            // 
+            this.txtMatricula.Location = new System.Drawing.Point(103, 58);
+            this.txtMatricula.Name = "txtMatricula";
+            this.txtMatricula.Size = new System.Drawing.Size(100, 20);
+            this.txtMatricula.TabIndex = 0;
+            // 
+            // txtSegundoParcial
+            // 
+            this.txtSegundoParcial.Location = new System.Drawing.Point(103, 213);
+            this.txtSegundoParcial.Name = "txtSegundoParcial";
+            this.txtSegundoParcial.Size = new System.Drawing.Size(100, 20);
+            this.txtSegundoParcial.TabIndex = 5;
+            // 
+            // txtAsignatura
+            // 
+            this.txtAsignatura.Location = new System.Drawing.Point(103, 151);
+            this.txtAsignatura.Name = "txtAsignatura";
+            this.txtAsignatura.Size = new System.Drawing.Size(100, 20);
+            this.txtAsignatura.TabIndex = 3;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(103, 120);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 2;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(103, 89);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(445, 460);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtAsignatura);
+            this.Controls.Add(this.txtSegundoParcial);
+            this.Controls.Add(this.txtMatricula);
+            this.Controls.Add(this.txtLaboratorio);
+            this.Controls.Add(this.txtPracticas);
+            this.Controls.Add(this.txtExamenFinal);
+            this.Controls.Add(this.txtNotaFinal);
+            this.Controls.Add(this.txtLiteral);
+            this.Controls.Add(this.txtCreditos);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPrimerParcial);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnBuscar);
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Windows Forms CRUD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtPrimerParcial;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtCreditos;
+        private System.Windows.Forms.TextBox txtLiteral;
+        private System.Windows.Forms.TextBox txtNotaFinal;
+        private System.Windows.Forms.TextBox txtExamenFinal;
+        private System.Windows.Forms.TextBox txtPracticas;
+        private System.Windows.Forms.TextBox txtLaboratorio;
+        private System.Windows.Forms.TextBox txtMatricula;
+        private System.Windows.Forms.TextBox txtSegundoParcial;
+        private System.Windows.Forms.TextBox txtAsignatura;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
     }
 }
 
--- LABSOF102/LABSOF102/Form1.cs
+++ LABSOF102/LABSOF102/Form1.cs
@@ -12,9 +12,73 @@ namespace LABSOF102
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public void LimpiarFormulario()
+        {
+            DialogResult respuesta = MessageBox.Show("¿Desea limpiar el formulario?", "Limpiar formulario", MessageBoxButtons.OKCancel);
+
+            if (respuesta == DialogResult.OK)
+            {
+                txtApellido.Clear();
+                txtAsignatura.Clear();
+                txtCreditos.Clear();
+                txtExamenFinal.Clear();
+                txtLaboratorio.Clear();
+                txtLiteral.Clear();
+                txtMatricula.Clear();
+                txtNotaFinal.Clear();
+                txtPracticas.Clear();
+                txtPrimerParcial.Clear();
+                txtSegundoParcial.Clear();
+                txtNombre.Clear();
+            }
+        }
+
+        public char CalcularLiteral()
+        {
+            if (Convert.ToInt32(txtNotaFinal.Text) >= 90)
+                return 'A';
+            else if (Convert.ToInt32(txtNotaFinal.Text) >= 80 && Convert.ToInt32(txtNotaFinal.Text) < 90)
+                return 'B';
+            else if (Convert.ToInt32(txtNotaFinal.Text) >= 70 && Convert.ToInt32(txtNotaFinal.Text) < 80)
+                return 'C';
+            else
+                return 'F';
+        }
+
+        public int CalcularNota()
+        {
+            try
+            {
+                return
+                    txtPrimerParcial.Text.Length == 0 ? 20 : Convert.ToInt32(txtPrimerParcial.Text) +
+                    txtSegundoParcial.Text.Length == 0 ? 0 : Convert.ToInt32(txtSegundoParcial.Text) +
+                    txtLaboratorio.Text.Length == 0 ? 0 : Convert.ToInt32(txtLaboratorio.Text) +
+                    txtPracticas.Text.Length == 0 ? 0 : Convert.ToInt32(txtPracticas.Text) +
+                    txtExamenFinal.Text.Length == 0 ? 0 : Convert.ToInt32(txtExamenFinal.Text);
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Uno o más de los campos contienen datos no válidos.", "Error");
+                return 0;
+            }
+        }
+
+            public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var response = MessageBox.Show("¿Desea salir de la aplicación?","Salir", MessageBoxButtons.YesNo);
+
+            if (response == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LimpiarFormulario();
         }
     }
 }
